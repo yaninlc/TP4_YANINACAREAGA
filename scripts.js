97eb2cc76d369f3851137b6465a91a4c91838f4a
yaninlc <lugoh7998@gmail.com>
2024-11-18 21:03:43 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("name-form");
     const welcomeMessage = document.getElementById("personalized-message");
 
+    // Si ya existe un nombre guardado, lo mostramos
+    const storedName = localStorage.getItem("userName");
+    if (storedName) {
+        welcomeMessage.textContent = `¡Hola, ${storedName}! Bienvenido a la página YNLC.`;
+        form.style.display = "none"; // Ocultamos el formulario si ya hay un nombre
+    }
+
     // Evento para manejar el ingreso del nombre
     form.addEventListener("submit", (event) => {
         event.preventDefault(); // Evita que el formulario se envíe
@@ -10,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         if (userName) {
             welcomeMessage.textContent = `¡Hola, ${userName}! Bienvenido a la página YNLC.`;  // Muestra el mensaje
             form.style.display = "none";  // Oculta el formulario
+            localStorage.setItem("userName", userName);  // Guardamos el nombre en el localStorage
         }
     });
 });
